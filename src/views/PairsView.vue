a1bad7c1df983433b0ebe128f4bdd636f8ffae7b
Ayguid <guidoaimar@gmail.com>
2024-04-30 00:43:33 -0300

    added charts

--- src/views/PairsView.vue
+++ src/views/PairsView.vue
@@ -2,10 +2,153 @@
 import { ref, watch, computed } from 'vue'
 //import { useRoute } from 'vue-router'
 import { state } from "@/sockets";
+import { Line } from 'vue-chartjs'
+import annotationPlugin from 'chartjs-plugin-annotation';
+import {  Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend
+} from 'chart.js'
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  annotationPlugin
+)
 //const route = useRoute()
 const loading = ref(false)
 const error = ref(null)
 const search = ref(null)
+//
+const stochChartData = computed(() => {
+  let indicatorsCharts = {};
+  let clone = JSON.parse(JSON.stringify(state.pairs)) // to avoid mutating state
+  clone.forEach(pair => {
+    let indicatorLabels = [];
+    let indicatorSTOCH = [];
+    let indicatorSTOCHk = [];
+    let indicatorSTOCHd = [];
+    pair.indicators.stoch_rsi.forEach((stochDatapoint, i) => {
+      indicatorLabels.push(i);
+      indicatorSTOCH.push(stochDatapoint.stochRSI)
+      indicatorSTOCHk.push(stochDatapoint.k)
+      indicatorSTOCHd.push(stochDatapoint.d)
+    });
+    indicatorsCharts[pair.key] = {
+      labels: indicatorLabels,
+      datasets: [
+        { label: 'stoch_rsi', backgroundColor: '#f44336', data: indicatorSTOCH, borderColor: '#f44336', tension: 0.2 , borderWidth: 2, pointRadius: 2, pointHoverRadius: 2},
+        { label: 'stoch_k', backgroundColor: '#36A2EB', data: indicatorSTOCHk, borderColor: '#36A2EB', tension: 0.2 , borderWidth: 2, pointRadius: 2, pointHoverRadius: 2 },
+        { label: 'stoch_d', backgroundColor: '#449848', data: indicatorSTOCHd, borderColor: '#449848', tension: 0.2 , borderWidth: 2, pointRadius: 2, pointHoverRadius: 2 },
+      ],
+      options: {//chart options 
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+          annotation: {
+            annotations: {
+              line1: {
+                type: 'line',
+                borderDash: [6, 6],
+                yMin: pair.stochBuyLimit,
+                yMax: pair.stochBuyLimit,
+                borderColor: '#FFFFFF',
+                borderWidth: 1,
+                label: {
+                  display: true,
+                  content: 'Stoch RSI limit ',//(ctx) => 'Average: ' + average(ctx).toFixed(2),
+                  position: 'start'
+                },
+              }
+            }
+          }
+        }
+      }
+    };
+  });
+  return indicatorsCharts;
+});
+
+const macdChartData = computed(() => {
+  let indicatorsCharts = {};
+  let clone = JSON.parse(JSON.stringify(state.pairs)) // to avoid mutating state
+  clone.forEach(pair => {
+    let indicatorLabels = [];
+    let indicatorSTOCH = [];
+    let indicatorSTOCHk = [];
+    let indicatorSTOCHd = [];
+    pair.indicators.macd.forEach((macdDatapoint, i) => {
+      indicatorLabels.push(i);
+      indicatorSTOCH.push(macdDatapoint.MACD)
+      indicatorSTOCHk.push(macdDatapoint.signal)
+      indicatorSTOCHd.push(macdDatapoint.histogram)
+    });
+    indicatorsCharts[pair.key] = {
+      labels: indicatorLabels,
+      datasets: [
+        { label: 'MACD', backgroundColor: '#f44336', data: indicatorSTOCH, borderColor: '#f44336', tension: 0.2, borderWidth: 2, pointRadius: 2, pointHoverRadius: 2 },
+        { label: 'signal', backgroundColor: '#36A2EB', data: indicatorSTOCHk, borderColor: '#36A2EB', tension: 0.2, borderWidth: 2, pointRadius: 2, pointHoverRadius: 2 },
+        { label: 'histogram', backgroundColor: '#449848', data: indicatorSTOCHd, borderColor: '#449848', tension: 0.2, borderWidth: 2, pointRadius: 2, pointHoverRadius: 2 },
+      ],
+      options: {//chart options 
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+          annotation: {
+            annotations: {
+              line1: {
+                type: 'line',
+                borderDash: [6, 6],
+                yMin: pair.macdBuyLimit,
+                yMax: pair.macdBuyLimit,
+                borderColor: '#FFFFFF',
+                borderWidth: 1,
+                label: {
+                  display: true,
+                  content: 'MACD limit ',//(ctx) => 'Average: ' + average(ctx).toFixed(2),
+                  position: 'start'
+                },
+              }
+            }
+          }
+        }
+      }
+    };
+  });
+  return indicatorsCharts;
+});
+
+const triggerChart = computed(() => {
+  let indicatorsCharts = {};
+  let clone = JSON.parse(JSON.stringify(state.pairs)) // to avoid mutating state
+  clone.forEach(pair => {
+    let indicatorLabels = ['', ''];
+    indicatorsCharts[pair.key] = {
+      labels: indicatorLabels,
+      datasets: [
+        { label: 'LowTrig', backgroundColor: '#f44336', data: [pair?.triggers?.downTrigger, pair?.triggers?.downTrigger], borderColor: '#f44336', tension: 0.2 },
+        { label: 'C price', backgroundColor: '#36a2eb', data: [pair.currentPrice.price, pair.currentPrice.price], borderColor: '#36a2eb', tension: 0.2, borderDash: [5, 5], },
+        { label: 'UpTrig', backgroundColor: '#449848', data: [pair?.triggers?.upTrigger, pair?.triggers?.upTrigger], borderColor: '#449848', tension: 0.2 },
+      ],
+      options: {//chart options 
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+        }
+      }
+    };
+  });
+  return indicatorsCharts;
+});
+
+
 // watch the params of the route to fetch the data again
 //watch(() => route.params.id, fetchData, { immediate: true })
 //computeds
@@ -32,7 +175,7 @@ const itemRowBackground = (item) => {
   return;
 }
 
-const readTimeStamp = (timestamp) =>{
+const readTimeStamp = (timestamp) => {
   return new Date(timestamp).toLocaleString();
 }
 </script>
@@ -50,31 +193,47 @@ const readTimeStamp = (timestamp) =>{
             <v-col v-for="item in items" :key="item.key" cols="12">
               <v-card>
                 <v-card-title class="d-flex align-center">
-                  <!--
-                    <v-icon :color="item.raw.color" :icon="item.raw.icon" size="18" start></v-icon>
-                  -->
                   <h4 class="py-2"><span class="text-amber">{{ item.raw.key }}</span> - <span
                       class="text-orange">CurrentPrice:
                     </span> {{
                       item.raw.currentPrice.price }} - <span class="text-orange">AvgPrice: </span>{{
                       item.raw.avgPrice.price }}</h4>
                 </v-card-title>
-
                 <v-card-text>
-                  <span class="text-green">STOCH RSI:&nbsp;</span>{{ item.raw.indicators.CURRENT_STOCH_RSI }}&nbsp;<span class="text-orange">Below limit:&nbsp;</span>{{ item.raw.indicators.CURRENT_STOCH_RSI.k < item.raw.stochBuyLimit}}
-                  <br> 
-                  <span class="text-green">MACD:&nbsp;</span>{{ item.raw.indicators.CURRENT_MACD}}&nbsp;<span class="text-orange">Below limit:&nbsp;</span>{{ item.raw.indicators.CURRENT_MACD.signal < item.raw.macdBuyLimit}}
-                  <br>
-                  <span class="text-green">ADX:&nbsp;</span>{{ item.raw.indicators.CURRENT_ADX}}&nbsp; 
-                  {{ (item.raw.indicators.CURRENT_ADX.adx + item.raw.indicators.CURRENT_ADX.pdi + item.raw.indicators.CURRENT_ADX.mdi)/3  }}
-                  {{ item.raw.indicators.CURRENT_ADX.pdi + item.raw.indicators.CURRENT_ADX.mdi  }}
-                  <br>
-                  <span v-if="item.raw.triggers"><span class="text-red">Down trigger:&nbsp;</span>{{ item.raw.triggers.downTrigger }}:&nbsp;<span class="text-green">Up trigger:&nbsp;</span>{{ item.raw.triggers.upTrigger }}</span>
-                  <span v-else>No active order -> No triggers</span>
+                  <v-row no-gutters>
+                    <v-col cols="12" sm="4">
+                      <Line v-if="stochChartData[item.raw.key]" :data="stochChartData[item.raw.key]"
+                        :options="stochChartData[item.raw.key].options" />
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <Line v-if="macdChartData[item.raw.key]" :data="macdChartData[item.raw.key]"
+                        :options="macdChartData[item.raw.key].options" />
+                    </v-col>
+                    <v-col v-if="item.raw.triggers" cols="12" sm="4">
+                      <Line v-if="triggerChart[item.raw.key]" :data="triggerChart[item.raw.key]"
+                        :options="triggerChart[item.raw.key].options" />
+                    </v-col>
+                  </v-row>
+                  <span class="text-green">STOCH RSI:&nbsp;</span>{{ item.raw.indicators.CURRENT_STOCH_RSI }}&nbsp;<span
+                    class="text-orange">Below limit:&nbsp;</span>{{ item.raw.indicators.CURRENT_STOCH_RSI.k <
+                      item.raw.stochBuyLimit }} {{ item.raw.stochBuyLimit }}<br>
+                    <span class="text-green">MACD:&nbsp;</span>{{ item.raw.indicators.CURRENT_MACD }}&nbsp;<span
+                      class="text-orange">Below limit:&nbsp;</span>{{ item.raw.indicators.CURRENT_MACD.signal <
+                        item.raw.macdBuyLimit }} {{ item.raw.macdBuyLimit }} <br>
+                      <span class="text-green">ADX:&nbsp;</span>{{ item.raw.indicators.CURRENT_ADX }}&nbsp;
+                      {{ (item.raw.indicators.CURRENT_ADX.adx + item.raw.indicators.CURRENT_ADX.pdi +
+                        item.raw.indicators.CURRENT_ADX.mdi) / 3 }}
+                      {{ item.raw.indicators.CURRENT_ADX.pdi + item.raw.indicators.CURRENT_ADX.mdi }}
+                      <br>
+                      <span v-if="item.raw.triggers"><span class="text-red">Down trigger:&nbsp;</span>{{
+                        item.raw.triggers.downTrigger }}:&nbsp;<span class="text-green">Up trigger:&nbsp;</span>{{
+                          item.raw.triggers.upTrigger }}</span>
+                      <span v-else>No active order -> No triggers</span>
                 </v-card-text>
 
                 <div class="px-4">
-                  <v-switch :label="`${isExpanded(item) ? 'Hide' : 'Show'} orders`" :model-value="isExpanded(item)" density="compact" inset @click="() => toggleExpand(item)"></v-switch>
+                  <v-switch :label="`${isExpanded(item) ? 'Hide' : 'Show'} orders`" :model-value="isExpanded(item)"
+                    density="compact" inset @click="() => toggleExpand(item)"></v-switch>
                 </div>
 
                 <v-divider></v-divider>
@@ -86,7 +245,8 @@ const readTimeStamp = (timestamp) =>{
                         <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify"
                           variant="outlined" hide-details single-line></v-text-field>
                       </template>
-                      <v-data-table :items="item.raw.orders" :search="search" calculate-widths :row-props="itemRowBackground">
+                      <v-data-table :items="item.raw.orders" :search="search" calculate-widths
+                        :row-props="itemRowBackground">
                         <template v-slot:item.side="{ value }">
                           <v-chip :color="getColor(value)">
                             {{ value }}
